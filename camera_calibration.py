af79afb1a20b31adbfcc7ab924988ffd754a5284
FedeNoce <federico.nocentini@stud.unifi.it>
2020-07-09 15:57:38 +0200

    add calibration matrix load function

--- camera_calibration.py
+++ camera_calibration.py
@@ -51,6 +51,20 @@ def save_coefficients(mtx, dist, path):
     cv_file.release()
 
 
+def load_coefficients(path):
+    """ Loads camera matrix and distortion coefficients. """
+    # FILE_STORAGE_READ
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+
+    # note we also have to specify the type to retrieve other wise we only get a
+    # FileNode object back instead of a matrix
+    camera_matrix = cv_file.getNode("K").mat()
+    dist_matrix = cv_file.getNode("D").mat()
+
+    cv_file.release()
+    return [camera_matrix, dist_matrix]
+
+
 if __name__ == '__main__':
     MATRIX_PATH = 'calibration_matrix.yml'
     mtx, dist = calibrate_camera()
