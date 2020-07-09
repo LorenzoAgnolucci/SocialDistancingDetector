33dd7a6d186ded6869165dcc10ce469183160a58
FedeNoce <federico.nocentini@stud.unifi.it>
2020-07-09 15:56:25 +0200

    implement camera calibration

---
+++ camera_calibration.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cv2
+import glob
+
+
+def calibrate_camera():
+    """Calibrate the camera with the given images and return the matrixes"""
+    IMAGES_PATH = f"images/*.jpg"
+    CHESSBOARD_WIDTH = 9
+    CHESSBOARD_HEIGHT = 6
+    
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((CHESSBOARD_HEIGHT * CHESSBOARD_WIDTH, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:CHESSBOARD_WIDTH, 0:CHESSBOARD_HEIGHT].T.reshape(-1, 2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+    images = glob.glob(IMAGES_PATH)
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_WIDTH, CHESSBOARD_HEIGHT), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret:
+            objpoints.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, - 1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (CHESSBOARD_WIDTH, CHESSBOARD_HEIGHT), corners2, ret)
+            cv2.imshow(fname, img)
+            cv2.waitKey(500)
+    cv2.destroyAllWindows()
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    return mtx, dist
+
+
+def save_coefficients(mtx, dist, path):
+    """ Save the camera matrix and the distortion coefficients to given path/file. """
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
+    cv_file.write("K", mtx)
+    cv_file.write("D", dist)
+    # note you *release* you don't close() a FileStorage object
+    cv_file.release()
+
+
+if __name__ == '__main__':
+    MATRIX_PATH = 'calibration_matrix.yml'
+    mtx, dist = calibrate_camera()
+    save_coefficients(mtx, dist, MATRIX_PATH)
