23165788683ffee6a02babeed3ae02170b8622d3
FedeNoce <federico.nocentini@stud.unifi.it>
2020-07-20 16:30:08 +0200

    fix a bug with webcam stream

--- VideoGet.py
+++ VideoGet.py
@@ -10,8 +10,9 @@ class VideoGet:
     with a dedicated thread.
     """
 
-    def __init__(self, src=0):
+    def __init__(self, src=0, webcamStream=False):
         self.src = src
+        self.webcamStream = webcamStream
         self.stream = cv2.VideoCapture(src)
         stream_fps = int(self.stream.get(cv2.CAP_PROP_FPS))
         stream_height = int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -30,9 +31,8 @@ class VideoGet:
         count_skip_frame = 0
         while not self.stopped:
             count_skip_frame += 1
-            if self.src == 0:
-                (self.grabbed, self.frame) = self.stream.read()
-            elif not self.queue.full():
+            (self.grabbed, self.frame) = self.stream.read()
+            if not self.queue.full():
                 if not self.grabbed:
                     self.stop()
                     return
@@ -40,13 +40,13 @@ class VideoGet:
                     self.queue.put(self.frame)
 
     def read(self):
-        if self.src == 0:
+        if self.src == 0 or self.webcamStream:
             return self.frame
         else:
             return self.queue.get()
 
     def more(self):
-        if self.src == 0:
+        if self.src == 0 or self.webcamStream:
             return True
         else:
             return self.queue.qsize() > 0
