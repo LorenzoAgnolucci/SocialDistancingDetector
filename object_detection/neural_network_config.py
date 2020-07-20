2fad94e03015659fd534654fe51a181d65234cdb
FedeNoce <federico.nocentini@stud.unifi.it>
2020-07-20 16:19:17 +0200

    add object detection code

---
+++ object_detection/neural_network_config.py
@@ -0,0 +1,11 @@
+# base path to YOLO directory
+MODEL_PATH = "yolo-coco"
+
+# initialize minimum probability to filter weak detections along with
+# the threshold when applying non-maxima suppression
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+
+# boolean indicating if NVIDIA CUDA GPU should be used
+USE_GPU = True
+
