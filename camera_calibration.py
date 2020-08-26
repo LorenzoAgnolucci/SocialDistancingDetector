dc458785818f1ebe6db7c8b9988a8bbe4a99fe5a
LorenzoAgnolucci <lory.agnolucci@gmail.com>
2020-08-26 11:14:46 +0200

    compute calibration error

--- camera_calibration.py
+++ camera_calibration.py
@@ -5,7 +5,6 @@ import glob
 
 def calibrate_camera():
     """Calibrate the camera with the given images and return the matrices"""
-    IMAGES_PATH = f"images/*.jpg"
     CHESSBOARD_WIDTH = 9
     CHESSBOARD_HEIGHT = 6
     
@@ -14,6 +13,7 @@ def calibrate_camera():
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((CHESSBOARD_HEIGHT * CHESSBOARD_WIDTH, 3), np.float32)
     objp[:, :2] = np.mgrid[0:CHESSBOARD_WIDTH, 0:CHESSBOARD_HEIGHT].T.reshape(-1, 2)
+
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
@@ -21,7 +21,7 @@ def calibrate_camera():
     for fname in images:
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        # _, gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
         # Find the chess board corners
         ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_WIDTH, CHESSBOARD_HEIGHT), None)
@@ -39,6 +39,13 @@ def calibrate_camera():
             cv2.waitKey(500)
     cv2.destroyAllWindows()
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        mean_error += error
+    print("total error: {}".format(mean_error / len(objpoints)))
     return mtx, dist
 
 
@@ -47,7 +54,7 @@ def save_coefficients(mtx, dist, path):
     cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
     cv_file.write("K", mtx)
     cv_file.write("D", dist)
-    # note you *release* you don't close() a FileStorage object
+
     cv_file.release()
 
 
@@ -55,8 +62,6 @@ def load_coefficients(path):
     """ Loads camera matrix and distortion coefficients. """
     cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
 
-    # note we also have to specify the type to retrieve other wise we only get a
-    # FileNode object back instead of a matrix
     camera_matrix = cv_file.getNode("K").mat()
     dist_matrix = cv_file.getNode("D").mat()
 
@@ -82,6 +87,12 @@ def get_calibrated_image(image, matrix_path):
 
 
 if __name__ == '__main__':
-    MATRIX_PATH = 'calibration_matrix.yml'
+    IMAGES_PATH = f"calibration_images/webcam_images/*.jpg"
+    MATRIX_PATH = 'webcam_calibration_matrix.yml'
     mtx, dist = calibrate_camera()
     save_coefficients(mtx, dist, MATRIX_PATH)
+    img = cv2.imread("calibration_images/webcam_images/image11.jpg")
+    cal = get_calibrated_image(img, MATRIX_PATH)
+    cv2.imshow("img", img)
+    cv2.imshow("cal", cal)
+    cv2.waitKey(0)
